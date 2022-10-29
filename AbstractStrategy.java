6252b84164be7ca403040ea5e48b878870b6492e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-10-28 23:33:15 -0700

    created a contstructor of AbstractStrategy class, now working on the implementation of the methods carried out.

--- AbstractStrategy.java
+++ AbstractStrategy.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public abstract class AbstractStrategy implements BoardStrategy {
     private JFrame frame;
-    final Container container;
-    final JPanel panelMancala;
+   Container container ;
+     JPanel panelMancala;
 
     private ArrayList<JButton> firstJButtons = new ArrayList<>();
     private ArrayList<JButton> secondJButtons = new ArrayList<>();
@@ -15,11 +15,54 @@ public abstract class AbstractStrategy implements BoardStrategy {
     private ArrayList<JButton> allButtons = new ArrayList<>();
     private JButton undoButton;
     private JButton quitButton;
-    private JLabel firstPlayerTurn, SecondPlayerTurn;
+    private JLabel firstPlayerTurn, secondPlayerTurn;
 
-    public AbstractStrategy(){
-        frame = new JFrame("Mancala Game");
-        frame.set
+    public AbstractStrategy()
+    {
+        //prepare frame
+        frame = new JFrame("Mancala Board");
+        frame.setSize(1200, 600);
+       container = frame.getContentPane();
+        container.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panelMancala = new JPanel(null);
+        CreateMancala();
+        CreateLabels();
+        CreateRowAPits();
+        CreateRowBPits();
+        insertButtons();
+        CreateLabelsForPlayerA();
+        undoButton = new JButton("UNDO");
+        quitButton = new JButton("QUIT");
+        undoButton.setBounds(10, 500, 75, 75);
+        quitButton.setBounds(1100, 500, 75, 75);
+        panelMancala.add(undoButton);
+        panelMancala.add(quitButton);
+        firstPlayerTurn = new JLabel("Player A -->");
+        secondPlayerTurn = new JLabel("<-- Player B ");
+        addPlayerTurns();
+
+    }
+
+    private void CreateMancala() {
+    }
+
+    private void CreateLabels() {
+    }
+
+    private void CreateRowAPits() {
+    }
+
+    private void CreateRowBPits() {
+    }
+
+    private void insertButtons() {
+    }
+
+    private void CreateLabelsForPlayerA() {
+    }
+
+    private void addPlayerTurns() {
     }
 
 }
