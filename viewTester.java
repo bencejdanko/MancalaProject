c3586284c14ea7ece6f8df8b33a17a79c2e517cd
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-10-28 22:19:52 -0700

    added a createMancala metod in viewTester which helps in creating the overall concept of the game but it requires the completion of AbstractStrategy class which will be built according to the needs.

--- viewTester.java
+++ viewTester.java
@@ -1,7 +1,32 @@
-public class viewTester {
+//right now focusing on View, for that an AbstractStrategy class will be required to maintain is-a relationship
+
+import javax.swing.*;
+
+public class viewTester extends AbstractStrategy {
 
 	public static void main(String[] args) {
 		Model model = new Model(4);
 		new View(model);
+
+
 	}
+	private JLabel firstPlayer;
+	private JLabel secondPlayer;
+
+	public void createMancala(){
+		String player = "A";
+		int y =80;
+
+		for(int i=0; i<2; i++){
+
+			//need to adda  getMancala method in AbstractStrategy
+			getMancala().add(new JButton());
+			getMancala().get(i).setBounds(320, y, 320, 40);
+			y= y+320;
+			getMancala().get(i).setName("Player" + player);
+			pnlMancala.add(getMancala().get(i));
+			player = "B";
+		}
+	}
+
 }
