0940358767ad31fc2124f3b3ca625781f6716b06
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-11-03 02:03:22 -0700

    added one addMancalaActionLisner for getting the inputs from player one and responding

--- Controller.java
+++ Controller.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Controller {
     private int counter;
     private Model model;
@@ -10,6 +14,21 @@ public class Controller {
         counter = 0;
         view.setRowAPits(model.getPitStones(0));
         view.setRowBPits(mode.get);
+
+        this.view.addMancalaActionListener(new ActionListener(){
+
+            public void actionPerformed(ActionEvent e){
+                String name = ((JButton) e.getSource()).getName();
+                if(name.equals("firstPlayer")){
+                    view.getMancala().get(0).setText(model.getStonesInP2Mancala()+ "");
+                }
+                else{
+                    view.getMancala().get(1).setText(model.getStonesInP1Mancala() + "");
+                }
+            }
+        });
+
+
     }
 }
   //
